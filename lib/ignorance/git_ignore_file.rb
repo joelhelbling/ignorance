464b4de715e0d7af2e8dd5eec8a74a3e569c95f5
Joel Helbling <joel@joelhelbling.com>
2013-02-10 18:06:56 -0500

    Git should also not act if token to ignore is in user's core.excludesfile

--- lib/ignorance/git_ignore_file.rb
+++ lib/ignorance/git_ignore_file.rb
@@ -9,6 +9,20 @@ module Ignorance
       @repo_dir    = '.git'
     end
 
+    private
+
+    def ignored
+      (file_contents + user_ignore_file_contents).reject{ |t| t.match /^\#/ }.map(&:chomp)
+    end
+
+    def user_ignore_file_contents
+      File.exists?(user_ignore_file) ? File.readlines(user_ignore_file) : []
+    end
+
+    def user_ignore_file
+      @user_ignore_file ||= `git config --global --get core.excludesfile`.chomp
+    end
+
   end
 end
 
--- spec/lib/ignorance/git_ignore_file_spec.rb
+++ spec/lib/ignorance/git_ignore_file_spec.rb
@@ -9,5 +9,21 @@ module Ignorance
 
     it_should_behave_like "an ignore file"
 
+    context "when the user's gitignore file includes the token" do
+      let(:token) { "foofile.md" }
+      let(:user_ignore_file) { '~/.gitignore' }
+      before do
+        ignorefile_write %w[other stuff here].join("\n")
+        File.open(user_ignore_file, 'w') do |fh|
+          fh.write "#{token}\n"
+        end
+        subject.stub(:user_ignore_file).and_return(user_ignore_file)
+      end
+
+      specify "then the file is ignored" do
+        File.read('~/.gitignore').should match /#{token}/
+        subject.ignored?(token).should be_true
+      end
+    end
   end
 end
